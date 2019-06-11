840e8acff80d823446e97daf68d6eb06bc50d6ec
Tiago Vila Verde <tiago.vilaverde@neotechnology.com>
2019-06-11 14:06:40 +0200

    handling uploading a receipt file

--- much-expenses/src/modules/Expenses/Item.jsx
+++ much-expenses/src/modules/Expenses/Item.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 
 import Comment from './Comment'
 
+import withFiniteStateMachine from '../../components/StateMachine/fsm.hoc'
+
 import { StyledItem } from './styles';
+import { assign } from 'xstate';
 
 const Expense = ({
   id,
@@ -15,12 +18,95 @@ const Expense = ({
   category,
   user,
   index,
+  submitReceipt,
+  componentState,
+  files,
+  send,
   onUpdate
 }) =>
   <StyledItem direction="column" across="start" along="center">
     <div>AMOUNT: {amount.value}{amount.currency}</div>
+    {
+      receipts.map(r => <img src={`http://localhost:3030${r.url}`} height="100" ></img>)
+    }
     <Comment id={id} content={comment} onUpdate={onUpdate} ></Comment>
+    <input name="receipt" type="file" onChange={evt => send('FILES.SELECTED', { files: evt.target.files })} />
+    {
+      files && files.length >= 0 ? <button onClick={evt => send('UPLOAD')}>Upload</button> : null
+    }  
   </StyledItem>
 
 
-export default Expense
+
+
+const expenseActions = props => ({
+  setFiles: assign({ files: (_, evt) => evt.files }),
+  updateHandler: () => props.onUpdate('REFRESH'),
+  logError: console.error
+})
+
+const expenseMachine = props => ({
+  id: `expense-${props.id}`,
+  initial: 'display',
+  context: {
+    collapsed: true,
+    files: []
+  },
+  states: {
+    display: {
+      initial: 'collapsed',
+      on: {
+        UPLOAD: 'uploading',
+        'FILES.SELECTED': {
+          target: 'display',
+          actions: ['setFiles']
+        }
+      },
+      states: {
+        opened: {
+          on: {
+            CLOSE: 'collapsed'
+          }
+        },
+        collapsed: {
+          on: {
+            OPEN: 'opened'
+          }
+        },
+      }
+    },
+    uploading: {
+      invoke: {
+        id: 'uploadReceipts',
+        src: (ctx, evt) => {
+          console.log('on uploadReceipts', ctx, evt)
+
+          const data = new FormData()
+          data.set('receipt', ctx.files[0], 'receipt')
+
+          return fetch(`http://localhost:3030/expenses/${props.id}/receipts`, { // Your POST endpoint
+            method: 'POST',
+            body: data
+          })
+            .then(response => response.json()) // if the response is a JSON object     
+            .then(console.log)
+            .catch(console.error);
+        },
+        onDone: {
+          target: 'display',
+          actions: ['updateHandler']
+        },
+        onError: {
+          target: 'display',
+          actions: ['logError']
+        }
+      }
+    }
+  }
+
+})
+
+export default withFiniteStateMachine(expenseMachine, expenseActions)(Expense)
+
+
+
