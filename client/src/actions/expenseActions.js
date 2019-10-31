9564a41383fdeb323f3057d454dc87bb812c128e
Kamran Elchuzade <elchuzade@gmail.com>
2019-10-31 16:35:13 +0300

    added getExpenses action, fixed reducers to the consistency, added loader css

--- client/src/actions/expenseActions.js
+++ client/src/actions/expenseActions.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
-import {
-  getError,
-  setLoading
-} from './commonActions';
+import { getError, setLoading } from './commonActions';
 
 import { GET_EXPENSES } from './types';
+
+export const getExpenses = () => dispatch => {
+  dispatch(setLoading('expenses'));
+  axios
+    .get('/expenses')
+    .then(res => {
+      dispatch({
+        type: GET_EXPENSES,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch(getError(err.response.data));
+    });
+};
---
+++ client/src/components/expenses/Expenses.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { getExpenses } from '../../actions/expenseActions';
+
+class Expenses extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {},
+      expenses: [],
+      total: ''
+    };
+  }
+  componentDidMount() {
+    this.props.getExpenses();
+  }
+  componentWillReceiveProps(nextProps) {
+    // Set errors
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+    // Set expenses
+    if (nextProps.expenses && nextProps.expenses.expenses) {
+      this.setState({
+        expenses: nextProps.expenses.expenses.expenses,
+        total: nextProps.expenses.expenses.total
+      });
+    }
+  }
+  render() {
+    const { errors, expenses } = this.state;
+    let spinner = null;
+    if (expenses.length === 0) {
+      spinner = <div className="loader" />;
+    } else {
+      spinner = null;
+    }
+    return (
+      <div>
+        {spinner}
+        {!spinner && (
+          <div className="container pt-5">{this.state.expenses[0].user.first}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+Expenses.propTypes = {
+  errors: PropTypes.object,
+  expenses: PropTypes.object,
+  getExpenses: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  errors: state.errors,
+  expenses: state.expenses
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    getExpenses
+  }
+)(Expenses);
