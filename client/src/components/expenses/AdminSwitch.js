0cb32581737f20de27ec326bcedb2a41bd183f2c
Kamran Elchuzade <elchuzade@gmail.com>
2019-11-03 18:20:53 +0300

    changed adminSwitch component from functional to class component, added locale functionality to it

--- client/src/components/expenses/AdminSwitch.js
+++ client/src/components/expenses/AdminSwitch.js
@@ -1,35 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
 
-const AdminSwitch = ({ admin, switchToAdmin }) => {
-  return (
-    <div className="text-center">
-      <button
-        className={classnames('btn-sm mx-1', {
-          'btn-secondary': admin,
-          'btn-outline-secondary': !admin
-        })}
-        onClick={e => switchToAdmin(e, true)}
-      >
-        employer
-      </button>
-      <button
-        className={classnames('btn-sm mx-1', {
-          'btn-secondary': !admin,
-          'btn-outline-secondary': admin
-        })}
-        onClick={e => switchToAdmin(e, false)}
-      >
-        employee
-      </button>
-    </div>
-  );
-};
+import { switchAdmin } from '../../actions/commonActions';
+
+class AdminSwitch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      admin: false,
+      language: 'ENG',
+      content: {}
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.locale && nextProps.locale.language) {
+      this.setState({
+        language: nextProps.locale.language,
+        content: nextProps.locale.content
+      });
+    }
+  }
+  switchToAdmin = (e, adminStatus) => {
+    e.preventDefault();
+    this.setState({ admin: adminStatus });
+    this.props.switchAdmin(adminStatus);
+  };
+  render() {
+    return (
+      <div className="text-center">
+        <button
+          className={classnames('btn-sm mx-1', {
+            'btn-secondary': this.state.admin,
+            'btn-outline-secondary': !this.state.admin
+          })}
+          onClick={e => this.switchToAdmin(e, true)}
+        >
+          {this.state.content.employer}
+        </button>
+        <button
+          className={classnames('btn-sm mx-1', {
+            'btn-secondary': !this.state.admin,
+            'btn-outline-secondary': this.state.admin
+          })}
+          onClick={e => this.switchToAdmin(e, false)}
+        >
+          {this.state.content.employee}
+        </button>
+      </div>
+    );
+  }
+}
 
 AdminSwitch.propTypes = {
-  admin: PropTypes.bool.isRequired,
-  switchToAdmin: PropTypes.func.isRequired
+  admin: PropTypes.object.isRequired,
+  locale: PropTypes.object.isRequired,
+  switchAdmin: PropTypes.func.isRequired
 };
 
-export default AdminSwitch;
+const mapStateToProps = state => ({
+  locale: state.locale,
+  admin: state.admin
+});
+
+export default connect(
+  mapStateToProps,
+  { switchAdmin }
+)(AdminSwitch);
