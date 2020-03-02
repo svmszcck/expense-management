b2767bc4e42e1df58bbe45e73094eeb098154a6a
Vitaliy Stanyshevskyy <vitaliy.stanyshevskyy@gmail.com>
2020-03-02 20:07:26 +0200

    Add icons to expenses list

--- frontend/src/components/expense/index.jsx
+++ frontend/src/components/expense/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
-import getSymbolFromCurrency from 'currency-symbol-map'
+import getSymbolFromCurrency from 'currency-symbol-map';
+import stringToColor from '../../helpers/string-to-color';
 import './index.scss';
 
 export default ({
@@ -22,9 +23,13 @@ export default ({
   const classNames = classnames('expense', {
     'expense--active': selected
   });
+  const color = stringToColor(merchant);
   return (
     <As className={classNames} onClick={onClick}>
       <div className='expense__container'>
+        <div className='expense_logo' style={{backgroundColor: color}}>
+          { merchant.charAt(0) }
+        </div>
         <div className='expense__info'>
           <p className='expense__merchant'>{ merchant.toLowerCase() }</p>
           <a className='expense__user' href={`mailto:${email}`}>{first} {last}</a>
