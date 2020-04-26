9bd563a3a2c79b4d3aecd9a1403439445f53aff6
Olha Afanasieva <afanasievaolja@gmail.com>
2020-04-26 21:43:12 +0200

    add Provider to routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 import HomePage from './pages/HomePage/HomePage';
 import ExpensesPage from './pages/ExpensesPage/ExpensesPage';
@@ -8,19 +10,21 @@ import ExpensePage from './pages/ExpensePage/ExpensePage';
 import './App.css';
 
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className='App'>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          {/* <Route exact path='/about' component={AboutPage} /> */}
-          <Route exact path='/expenses' component={ExpensesPage} />
-          <Route exact path='/expenses/:id' component={ExpensePage} />
-          {/* <Route exact path='/*' component={ErrorPage} /> */}
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            {/* <Route exact path='/about' component={AboutPage} /> */}
+            <Route exact path='/expenses' component={ExpensesPage} />
+            <Route exact path='/expenses/:id' component={ExpensePage} />
+            {/* <Route exact path='/*' component={ErrorPage} /> */}
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
