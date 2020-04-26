a1daa138abe60f9260833bdf7fa6d0064a1c2f21
Olha Afanasieva <afanasievaolja@gmail.com>
2020-04-26 22:17:48 +0200

    render header of table of expenses

--- client/src/components/ListOfExpenses/ListOfExpenses.tsx
+++ client/src/components/ListOfExpenses/ListOfExpenses.tsx
@@ -1,20 +1,32 @@
 import React from "react";
+import { Expense } from "../../types/Expense";
+import ExpenseItem from '../ExpenseItem/ExpenseItem';
+import './list.scss';
 
 interface IListOfExpensesProps {
-  expenses: Array<any>
+  expenses: Expense[],
+  total: number
 };
 
 class ListOfExpenses extends React.Component<IListOfExpensesProps> {
   render() {
-    console.log(this.props)
     return (
       <div className="list-of-expenses">
         <table className="list">
+          <thead>
+            <tr>
+              <th>Amount</th>
+              <th>Date</th>
+              <th>Merchant</th>
+              <th>User</th>
+              <th>Receipts</th>
+              <th>Comments</th>
+              <th></th>
+            </tr>
+          </thead>
           <tbody>
             {this.props.expenses.map(item => {
-              return <tr>
-                <td>{item.id}</td>
-              </tr>
+              return <ExpenseItem key={item.id} data={item} />;
             })}
           </tbody>
         </table>
