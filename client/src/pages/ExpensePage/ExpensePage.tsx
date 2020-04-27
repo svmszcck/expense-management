af67d1ad6ebecf4fd008bd9f40062f3349803d96
Olha Afanasieva <afanasievaolja@gmail.com>
2020-04-27 21:32:33 +0200

    fetching data for single Expense on the Expemse Page. Rendering data

--- client/src/pages/ExpensePage/ExpensePage.tsx
+++ client/src/pages/ExpensePage/ExpensePage.tsx
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Expense } from '../../types/Expense';
+import moment from 'moment';
 
-class ExpensePage extends Component {
-    render() {
-        return (
-            <>Expense</>
-        )
-    }
+interface IExpensePageProps {
+};
+
+interface IExpensePageState {
+  id: string,
+  currentExpense?: Expense | null
 }
+
+class ExpensePage extends Component<IExpensePageProps & RouteComponentProps, IExpensePageState> {
+  state: IExpensePageState = {
+    id: '',
+    currentExpense: null
+  }
+
+  async componentDidMount() {
+    const id: string = await (this.props.match.params as any).id;
+    this.setState({ id });
+    this.fetchExpense(this.state.id)
+  }
+
+  fetchExpense = async (id: string) => {
+    const response = await fetch(`http://localhost:3000/expenses/${id}`);
+    const currentExpense = await response.json();
+    this.setState({ currentExpense });
+  };
+
+  render() {
+    let merchant, date, amount, userName, userEmail, receipts, comment, category;
+    if (this.state.currentExpense && Object.entries(this.state.currentExpense).length > 0) {
+      merchant = this.state.currentExpense.merchant;
+      date = moment(this.state.currentExpense.date).format("L");
+      amount = `${this.state.currentExpense.amount.value} ${this.state.currentExpense.amount.currency}`;
+      userName = `${this.state.currentExpense.user.first} ${this.state.currentExpense.user.last}`;
+      userEmail = this.state.currentExpense.user.email;
+      receipts = this.state.currentExpense.receipts;
+      comment = this.state.currentExpense.comment || "-";
+      category = this.state.currentExpense.category || "-";
+    }
+
+    return (
+      <>
+      Expense
+      <p>Date: {date}</p>
+        <p>Amount: {amount}</p>
+        <p>Merchant: {merchant}</p>
+        <p>User: {userName}</p>
+        <p>User email: {userEmail}</p>
+        <p>Category: {category}</p>
+      </>
+    )
+  }
+};
+
 export default ExpensePage;
